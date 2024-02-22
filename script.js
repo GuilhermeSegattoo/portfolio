9e38893575972a3315c996ca2e622436b1b1ed8b
Guilherme Segatto <segattooguilherme@gmail.com>
2024-02-21 23:07:10 -0300

    section Projetos 1.0

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  if (n >= slides.length) { slideIndex = 0 }
+  if (n < 0) { slideIndex = slides.length - 1 }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+}
+
+function prevSlide() {
+  changeSlide(-1);
+}
+
+function nextSlide() {
+  changeSlide(1);
+}
